c915c189526b813a9d48de513cff966000ce7fe8
Stomni <jackisch.stefan@gmail.com>
2022-03-27 16:21:57 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css';
+import "./App.css";
 import DemoraMap from "./components/DemoraMap";
 
 import "./components/DemoraMap.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-
   return (
-    <DemoraMap>
-
-    </DemoraMap>
+    <>
+      <NavBar />
+      <DemoraMap />
+    </>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,22 @@
+import "./NavBar.css";
+
 export function NavBar() {
-    return <nav>
-        
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Continents</a>
+        </li>
+        <li>
+          <a href="/">Glossar</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+      </ul>
     </nav>
+  );
 }
