600eb76f6c5d548ca0ffb5cead0e85355ebd34a4
Stomni <jackisch.stefan@gmail.com>
2023-01-22 19:57:18 +0100

    added download link for dnd ultima

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,6 +5,7 @@ export function Footer() {
     return <footer className="footer">
     <div className="footer-links">
       <a href="https://app.fantasy-calendar.com/calendars/ac78c08c5c9afd26255cde628f85e16e" target="_blank" rel="noreferrer">Telos Calender</a>
+      <a href="https://drive.google.com/file/d/13VKXYTvgVYxUG6CrFW36p1VSSxRPaSdT/view?usp=share_link" target="_blank" rel="noreferrer">DnD Ultima App Private Version</a>
     </div>
   </footer>
 }
