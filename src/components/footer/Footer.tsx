8228f58f1e85ac82af4b75c33951b6dfbf552c37
Stomni <jackisch.stefan@gmail.com>
2023-01-24 13:14:59 +0100

    new link for dnd ultima footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     return <footer className="footer">
     <div className="footer-links">
       <a href="https://app.fantasy-calendar.com/calendars/ac78c08c5c9afd26255cde628f85e16e" target="_blank" rel="noreferrer">Telos Calender</a>
-      <a href="https://drive.google.com/file/d/13VKXYTvgVYxUG6CrFW36p1VSSxRPaSdT/view?usp=share_link" target="_blank" rel="noreferrer">DnD Ultima App Private Version</a>
+      <a href="https://drive.google.com/drive/u/0/folders/1_JfStZnduxQ8EVM1M5ZhaK-xCYtWkF5G" target="_blank" rel="noreferrer">DnD Ultima App Private Version</a>
     </div>
   </footer>
 }
